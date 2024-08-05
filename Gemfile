59f196a89a3b08adeab8660c54074329b82c82ab
VivianaS14 <vasorianp@gmail.com>
2024-08-05 16:14:55 -0500

    feat: friendly id to show in url

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem "bootsnap", require: false
 gem 'devise', '~> 4.9', '>= 4.9.4'
 # https://rubydoc.info/github/heartcombo/devise#getting-started
 
+gem 'friendly_id', '~> 5.5', '>= 5.5.1'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_01_203705) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_05_163408) do
   create_table "trees", force: :cascade do |t|
     t.string "name"
     t.string "x"
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_01_203705) do
     t.string "style"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_trees_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
